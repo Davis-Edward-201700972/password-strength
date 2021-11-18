cd1bfff4777bcdeac1e9992cdba65f096d40a1f8
Davis Edward <2938836290101@ingenieria.usac.edu.gt>
2021-11-18 12:07:26 -0600

    Fix

--- src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of password-strength
- */
-
-export * from './lib/enum';
-export * from './lib/component';
-export * from './lib/animations';
-export * from './lib/validator';
-export * from './lib/mat-password-strength.module';
